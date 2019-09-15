19239a316559c5efcf36211b90980e71c4eefb72
Kell <kellspam@outlook.fr>
2019-09-15 02:39:58 +0200

    Better code, Emerald Coast act swap fix

--- sadx-chao-gameplay/mod.cpp
+++ sadx-chao-gameplay/mod.cpp
@@ -8,7 +8,15 @@ uint8_t SelectedChao = 0;
 bool isloaded = false;
 
 std::vector<NJS_PLANE> waterlist = {};
-bool checkwater = false;
+
+int GetCurrentChaoStage_r() {
+	if (ChaoObject) return 5;
+	else return CurrentChaoStage;
+}
+
+inline ChaoData* GetChaoData(uint8_t id) {
+	return (ChaoData *)(GetChaoSaveAddress() + 2072 + (2048 * id));
+}
 
 void SelectChao() {
 	CharObj2 * co2 = GetCharObj2(0);
@@ -21,7 +29,7 @@ void SelectChao() {
 
 			if (chaodata->entity.CollisionInfo->CollisionArray->origin.y == 2) {
 				for (uint8_t i = 0; i < 24; ++i) {
-					ChaoData * tempdata = (ChaoData *)(GetChaoSaveAddress() + 2072 + (2048 * i));
+					ChaoData * tempdata = GetChaoData(i);
 					if (chaodata->ChaoDataBase_ptr
 						&& tempdata->data.Lifespan == chaodata->ChaoDataBase_ptr->Lifespan
 						&& tempdata->data.DNA.FavoriteFruit1 == chaodata->ChaoDataBase_ptr->DNA.FavoriteFruit1
@@ -37,137 +45,124 @@ void SelectChao() {
 	}
 }
 
-int GetCurrentChaoStage_r() {
-	if (ChaoObject) return 5;
-	else return CurrentChaoStage;
+void GetWaterCollisions() {
+	waterlist.clear();
+	for (int i = 0; i < CurrentLandTable->COLCount; ++i) {
+		if (CurrentLandTable->Col[i].Flags & ColFlags_Water) {
+
+			float x_min = CurrentLandTable->Col[i].Center.x - CurrentLandTable->Col[i].Radius;
+			float z_min = CurrentLandTable->Col[i].Center.z - CurrentLandTable->Col[i].Radius;
+			float x_max = CurrentLandTable->Col[i].Center.x + CurrentLandTable->Col[i].Radius;
+			float z_max = CurrentLandTable->Col[i].Center.z + CurrentLandTable->Col[i].Radius;
+			float y = CurrentLandTable->Col[i].Center.y;
+
+			NJS_PLANE temp = { x_min, y, z_min, x_max, 0, z_max };
+			waterlist.push_back(temp);
+		}
+	}
 }
 
-void __cdecl InitLandTableObject_r(NJS_OBJECT *a1)
-{
-	checkwater = false;
-
-	NJS_OBJECT *v1 = a1;
-	do
-	{
-		Uint32 v2 = v1->evalflags;
-		Uint32 v6 = v1->evalflags;
-		if (!(v1->evalflags & 0x8))
-		{
-			NJS_MODEL_SADX *v3 = v1->basicdxmodel;
-			if (v3)
-			{
-				int v4 = v3->nbMeshset;
-				NJS_MESHSET_SADX *v5 = v3->meshsets;
-				if (v3->nbMeshset)
-				{
-					do
-					{
-						InitLandTableMeshSet(v3, v5);
-						++v5;
-						--v4;
-					} while (v4);
-					v2 = v6;
-				}
-			}
-		}
-		if (!(v2 & 0x10))
-		{
-			InitLandTableObject(v1->child);
-		}
-		v1 = v1->sibling;
-	} while (v1);
-}
-
-void Chao_SwimInWater(ObjectMaster * a1) {
+void IsChaoInWater(ObjectMaster * a1) {
 	ChaoData1 * chaodata1 = (ChaoData1*)a1->Data1;
 	ChaoData2 * chaodata2 = (ChaoData2*)a1->Data2;
 
-	if (!checkwater) {
-		waterlist.clear();
-		for (int i = 0; i < CurrentLandTable->COLCount; ++i) {
-			if (CurrentLandTable->Col[i].Flags & ColFlags_Water) {
-
-				float x_min = CurrentLandTable->Col[i].Center.x - CurrentLandTable->Col[i].Radius;
-				float z_min = CurrentLandTable->Col[i].Center.z - CurrentLandTable->Col[i].Radius;
-				float x_max = CurrentLandTable->Col[i].Center.x + CurrentLandTable->Col[i].Radius;
-				float z_max = CurrentLandTable->Col[i].Center.z + CurrentLandTable->Col[i].Radius;
-				float y = CurrentLandTable->Col[i].Center.y;
-
-				NJS_PLANE temp = { x_min, y, z_min, x_max, 0, z_max };
-				waterlist.push_back(temp);
-			}
-		}
-		checkwater = true;
-	}
-	else {
-		float height = -10000000;
-		if (waterlist.size() > 0) {
-			NJS_VECTOR pos = chaodata1->entity.Position;
-			NJS_PLANE wpos;
-			for (int i = 0; i < waterlist.size(); ++i) {
-				wpos = waterlist[i];
-				if (pos.y < wpos.py + 2 && pos.y > wpos.py - 170) {
-					if (pos.x > wpos.px && pos.x < wpos.vx) {
-						if (pos.z > wpos.pz && pos.z < wpos.vz) {
-							height = wpos.py;
-						}
+	float height = -10000000;
+	if (waterlist.size() > 0) {
+		NJS_VECTOR pos = chaodata1->entity.Position;
+		NJS_PLANE wpos;
+		for (int i = 0; i < waterlist.size(); ++i) {
+			wpos = waterlist[i];
+			if (pos.y < wpos.py + 2 && pos.y > wpos.py - 170) {
+				if (pos.x > wpos.px && pos.x < wpos.vx) {
+					if (pos.z > wpos.pz && pos.z < wpos.vz) {
+						height = wpos.py;
 					}
 				}
 			}
 		}
-		WriteData((float*)0x73C24C, height);
-	}
-}
-
-#pragma region Own Chao Object
-void ChaoObj_Main(ObjectMaster * a1) {
-	if (a1->Data1->Action == 0) {
-		ChaoData * chaodata = (ChaoData *)(GetChaoSaveAddress() + 2072 + (2048 * (SelectedChao - 1))); //get chao data
-
-		NJS_VECTOR v = EntityData1Ptrs[0]->Position;
-		v.x -= 20;
-
-		CurrentChao = CreateChao(chaodata, 0, CurrentChao, &v, 0);
-		if (EntityData1Ptrs[0]->Action != 12) SetHeldObject(0, CurrentChao);
-		a1->Data1->Action = 1;
 	}
 
-	Chao_SwimInWater(CurrentChao); //allow chaos to swim in any water colision
+	WriteData((float*)0x73C24C, height);
 }
 
 void ChaoObj_Delete(ObjectMaster * a1) {
 	DeleteObjectMaster(ChaoManager);
 	ChaoManager = nullptr;
+
 	DeleteObjectMaster(CurrentChao);
 	CurrentChao = nullptr;
 	ChaoObject = nullptr;
+
+	//Release the chao textures
 	FreeChaoTexlists();
+
+	//reset default water height
 	float height = 0;
 	WriteData((float*)0x73C24C, height);
-	checkwater = false;
 }
 
-void ChaoObj_Init(ObjectMaster * a1) {
-	LoadChaoTexlist("AL_DX_PARTS_TEX", (NJS_TEXLIST*)0x33A1340, 0);
-	LoadChaoTexlist("AL_BODY", ChaoTexLists, 0);
-	LoadChaoTexlist("AL_jewel", &ChaoTexLists[4], 0);
-	LoadChaoTexlist("AL_ICON", &ChaoTexLists[3], 0);
-	LoadChaoTexlist("AL_EYE", &ChaoTexLists[2], 0);
-	LoadChaoTexlist("AL_MOUTH", &ChaoTexLists[5], 0);
-	LoadChaoTexlist("AL_TEX_COMMON", &ChaoTexLists[1], 1u);
+void ChaoObj_Main(ObjectMaster * a1) {
+	uint8_t Action = a1->Data1->Action;
+	
+	if (Action == 0) {
+		//We wait a bit before loading chao stuff
+		if (!CurrentLandTable) return;
 
-	if (!isloaded) {
-		al_confirmload_load();
-		LoadChaoPVPs();
-		isloaded = true;
+		//Load the chao textures
+		LoadChaoTexlist("AL_DX_PARTS_TEX", (NJS_TEXLIST*)0x33A1340, 0);
+		LoadChaoTexlist("AL_BODY", ChaoTexLists, 0);
+		LoadChaoTexlist("AL_jewel", &ChaoTexLists[4], 0);
+		LoadChaoTexlist("AL_ICON", &ChaoTexLists[3], 0);
+		LoadChaoTexlist("AL_EYE", &ChaoTexLists[2], 0);
+		LoadChaoTexlist("AL_MOUTH", &ChaoTexLists[5], 0);
+		LoadChaoTexlist("AL_TEX_COMMON", &ChaoTexLists[1], 1u);
+
+		//PVPs only need to be loaded once
+		if (!isloaded) {
+			al_confirmload_load();
+			LoadChaoPVPs();
+			isloaded = true;
+		}
+
+		ChaoManager_Load(); //Load chao behaviour
+		GetWaterCollisions(); //Hacky solution to make chao swim
+
+		ActCopy = CurrentAct;
+
+		a1->DeleteSub = ChaoObj_Delete; //When you quit a level
+		a1->Data1->Action = 1; //Wait a frame before loading a chao
 	}
+	else if (Action == 1) {
+		//We get the chao data in the savefile
+		ChaoData* chaodata = GetChaoData(SelectedChao - 1);
 
-	ChaoManager_Load(); //load chao behaviour
+		//Start position is behind the player
+		NJS_VECTOR v = EntityData1Ptrs[0]->Position;
+		v.x -= 20;
 
-	a1->MainSub = ChaoObj_Main; //everyframe except when game paused
-	a1->DeleteSub = ChaoObj_Delete; //when you quit a level
+		//Load the chao
+		CurrentChao = CreateChao(chaodata, 0, CurrentChao, &v, 0);
+		if (EntityData1Ptrs[0]->Action != 12) SetHeldObject(0, CurrentChao);
+
+		a1->Data1->Action = 2;
+	}
+	else {
+		//If the act has changed, check water collisions again
+		if (ActCopy != CurrentAct) {
+			GetWaterCollisions();
+			ActCopy = CurrentAct;
+
+			//Fix a bug at Emerald Coast act swap by reloading the chao
+			if (CurrentLevel == LevelIDs_EmeraldCoast && CurrentAct == 1) {
+				a1->DeleteSub(a1);
+				a1->Data1->Action = 0;
+				return;
+			}
+		}
+
+		IsChaoInWater(CurrentChao);
+	}
 }
-#pragma endregion
 
 extern "C"
 {
@@ -176,8 +171,9 @@ extern "C"
 		const IniFile *config = new IniFile(std::string(path) + "\\config.ini");
 		delete config;
 
+		//Trick the game into thinking we're in a specific chao garden
+		//Needed to change the water height
 		WriteJump((void*)0x715140, GetCurrentChaoStage_r);
-		WriteJump((void*)0x787140, InitLandTableObject_r);
 	}
 
 	__declspec(dllexport) void __cdecl OnFrame()
@@ -185,7 +181,7 @@ extern "C"
 		if ((CurrentLevel >= LevelIDs_StationSquare && CurrentLevel <= LevelIDs_Past) || IsLevelChaoGarden()) SelectChao();
 
 		if ((GameState == 4 || GameState == 2) && SelectedChao && !ChaoObject && !IsLevelChaoGarden())
-			ChaoObject = LoadObject((LoadObj)(LoadObj_Data1), 1, ChaoObj_Init);
+			ChaoObject = LoadObject((LoadObj)(LoadObj_Data1), 1, ChaoObj_Main);
 	}
 
 	__declspec(dllexport) ModInfo SADXModInfo = { ModLoaderVer };
