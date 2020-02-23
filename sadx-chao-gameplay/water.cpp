7c1e72f2b1f03e6d79aab496555b37b3c4464d80
Kell <kellspam@outlook.fr>
2020-02-23 14:39:40 +0100

    Check for water if not held, fixes

--- sadx-chao-gameplay/water.cpp
+++ sadx-chao-gameplay/water.cpp
@@ -5,10 +5,11 @@
 void IsChaoInWater(ObjectMaster* obj) {
 	ChaoData1* chaodata1 = (ChaoData1*)obj->Data1;
 
-	float height = -10000000;
+	WriteData<5>((void*)0x49F201, 0x90);
+	WriteData<5>((void*)0x49F1C0, 0x90);
 
 	struct_a3 dyncolinfo;
-	WriteData<5>((void*)0x49F201, 0x90);
+	float height = -10000000;
 	RunEntityIntersections(&chaodata1->entity, &dyncolinfo);
 
 	if (dyncolinfo.ColFlagsA & (0x400002 | ColFlags_Water)) {
@@ -21,14 +22,17 @@ void IsChaoInWater(ObjectMaster* obj) {
 	}
 
 	WriteCall((void*)0x49F201, SpawnRipples);
+	WriteCall((void*)0x49F1C0, SpawnSplashParticles);
+
 	WriteData((float*)0x73C24C, height);
 }
 
-//Different water height for each chao
+//Different water height for each chao in levels to swim in any water collision.
+//Don't check if held or following player.
 void Chao_Main_r(ObjectMaster* obj);
 Trampoline Chao_Main_t((int)Chao_Main, (int)Chao_Main + 0x6, Chao_Main_r);
 void Chao_Main_r(ObjectMaster* obj) {
-	if (CurrentLevel < LevelIDs_SSGarden) {
+	if (CurrentLevel < LevelIDs_SSGarden && (obj->Data1->Status & 0x1000) == 0 && obj->Data1->CharIndex != 1) {
 		IsChaoInWater(obj);
 	}
 
